8a57bf5cc8dfff7554a840bf02ecfc89bf995fee
funny匚 <50166662+funny0628@users.noreply.github.com>
2020-02-28 22:24:35 +0800

    Update animate.js
    update 缓动js

--- animate.js
+++ animate.js
@@ -1,23 +1,43 @@
+//匀速运动
 function animationMove(ele, target) {
     clearInterval(ele.timeID)
 
     ele.timeID = setInterval(() => {
+        //1.获取元素当前位置
         var currentLeft = ele.offsetLeft;
-        //判断元素的移动方向
+        //2.判断元素的移动方向
         var isleft = currentLeft < target ? true : false
-
         isleft ? currentLeft += 10 : currentLeft -= 10;
-        // isleft ? current += 10 : current -= 10;
+        //开始移动
         ele.style.left = currentLeft + 'px';
-        //终点检测
-
+        //3.终点检测
         if (isleft ? currentLeft >= target : currentLeft <= target) {
             //清除定时器
             clearInterval(ele.timeID);
-            //边界检测
+            //4.边界检测
             ele.style.left = target + 'px';
         }
 
+    }, 10);
+}
 
+//缓动运动
+function slowanimationMove(ele, target) {
+    clearInterval(ele.timeID)
+    ele.timeID = setInterval(() => {
+        //1.获取元素当前位置
+        var currentLeft = ele.offsetLeft;
+        //2.计算每次移动距离,由(目标距离 - 当前位置) / 10 因为使用的除法后面会有小数,所以要向上取整
+        var step = (target - currentLeft) / 10;
+        //取整
+        step = Math.ceil(step)
+        //3.开始移动
+        currentLeft += step
+        ele.style.left = currentLeft + 'px';
+        //4.终点检测,因为上面的除法已经向上取整了,所以最终一定是会和目标距离相等的,所以就没有了终点检测,只是清除了定时器,
+        if (currentLeft === target) {
+            //清除定时器
+            clearInterval(ele.timeID);
+        }
     }, 10);
 }
