da5618ed672586ebcf3948679a8534aeaa82d37e
funny匚 <50166662+funny0628@users.noreply.github.com>
2020-02-29 16:42:21 +0800

    update 支持透明度, 层级 和回调函数的封装

--- animate.js
+++ animate.js
@@ -41,3 +41,57 @@ function slowanimationMove(ele, target) {
         }
     }, 10);
 }
+
+
+
+//不限制元素属性个数,并且可以支持 透明度,层级,和回调函数的缓动动画封装
+function animationslow(ele, attrs, fn) {
+        //清除定时器
+        clearInterval(ele.timer)
+        ele.timer = setInterval(() => {
+            var isall = true
+            for (var key in attrs) {
+                console.log(key, attrs[key]);
+                var attribute = key;
+                var target = attrs[key];
+                //1.排除层级
+                if (attribute === 'zIndex') {
+                    ele.style[attribute] = target;
+                } else if (attribute === 'opacity') {//排除透明度
+                    //1.获取元素当前的位置
+                    var currentAttribute = parseFloat(window.getComputedStyle(ele, null)[attribute]);
+                    currentAttribute *= 100;
+                    target *= 100
+                    //2.计算每次移动的距离
+                    var step = (target - currentAttribute) / 10;
+                    //取整
+                    step = step > 0 ? Math.ceil(step) : Math.floor(step);
+                    currentAttribute += step;
+                    ele.style[attribute] = currentAttribute / 100;
+                    //4.终点检测
+                    if (currentAttribute !== target) {
+                        isall = false
+                    }
+                } else {
+                    //1.获取元素当前的位置
+                    var currentAttribute = parseInt(window.getComputedStyle(ele, null)[attribute]);
+                    //2.计算每次移动的距离
+                    var step = (target - currentAttribute) / 10;
+                    //取整
+                    step = step > 0 ? Math.ceil(step) : Math.floor(step);
+                    currentAttribute += step;
+                    ele.style[attribute] = currentAttribute + 'px';
+                    //4.终点检测
+                    if (currentAttribute !== target) {
+                        isall = false
+                    }
+                }
+            }
+            if (isall === true) {
+                clearInterval(ele.timer);
+                if (typeof fn === "function") {
+                    fn()
+                }
+            }
+        }, 50);
+    }
